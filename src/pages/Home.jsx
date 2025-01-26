220fd05c5f992323dd88124d6153c99af58be82e
AshishNith <ranjan.ashish9992@gmail.com>
2025-01-26 23:21:16 +0530

    Logo Fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,9 +22,9 @@ const Home = () => {
       {/* Hero Section */}
 
         <section className='relative w-full lg:min-h-screen min-h-[70vh] flex flex-col justify-center items-center '>
-      <img src={logo} alt="" className=' lg:absolute lg:top-[-10px] lg:right-[30px] absolute scale-0 lg:scale-100  lg:w-[150px] '/>
+         <img src={logo} alt="" className=' lg:absolute lg:top-[-10px] lg:right-[30px] absolute top-2 p-1 backdrop-blur-sm w-[100px] border border-white/20 rounded-full  lg:scale-100  lg:w-[150px] '/>
         <div className='flex flex-col justify-center items-center space-y-8 gap-205'>
-          <h1 className='relative z-[100] text-white/80 font-squidFont lg:text-[8vw] lg:leading-[8vw] leading-[14vw] text-[14vw] text-center '>ROBOWEEK 3.0</h1>
+          <h1 className='relative z-[100] text-white/80 font-squidFont lg:text-[8vw] mt-20 lg:leading-[8vw] leading-[14vw] text-[14vw] text-center '>ROBOWEEK 3.0</h1>
 
           <h2 className='font-squidFont text-white text-2xl lg:text-3xl'>MYRAID OF iNNOVATION</h2>
 
